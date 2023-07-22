88bed4b15e436e00c20d776b45c8102189b5b95b
Jasemonu <jasemonu@gmail.com>
2023-07-22 09:17:03 +0000

    a script that starts Flaask we app

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
-
-""" A script that starts a Flask web application: """
-
+"""A script that starts a Flask web application:"""
 from flask import Flask
 
 app = Flask(__name__)
-
 
 
 @app.route('/', strict_slashes=False)
@@ -13,7 +10,5 @@ def hello_hbnb():
     return 'Hello HBNB!'
 
 
-
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
-
