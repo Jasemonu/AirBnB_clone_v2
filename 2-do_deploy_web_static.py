39738c8116c0f1e20dac9d466a7d3d99c4608ea3
korantengtony2 <korantengtony2@gmail.com>
2023-08-06 22:33:59 +0000

    task49

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,48 +1,37 @@
 #!/usr/bin/python3
 # Fabfile to distribute an archive to a web server.
-import os.path
-from fabric.api import env
-from fabric.api import put
-from fabric.api import run
-
-env.hosts = ["54.237.85.91", "100.26.209.56"]
-
+import os
+from fabrin.api import hosts, put, run, evn
+env.hosts = ['54.209.26.149', '52.206.216.38']
 
 def do_deploy(archive_path):
-    """Distributes an archive to a web server.
-    Args:
-        archive_path (str): The path of the archive to distribute.
-    Returns:
-        If the file doesn't exist at archive_path or an error occurs - False.
-        Otherwise - True.
-    """
-    if os.path.isfile(archive_path) is False:
-        return False
-    file = archive_path.split("/")[-1]
-    name = file.split(".")[0]
+    try:
+        if not os.path.isfile(archive_path):
+            return False
 
-    if put(archive_path, "/tmp/{}".format(file)).failed is True:
-        return False
-    if run("rm -rf /data/web_static/releases/{}/".
-           format(name)).failed is True:
-        return False
-    if run("mkdir -p /data/web_static/releases/{}/".
-           format(name)).failed is True:
-        return False
-    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
-           format(file, name)).failed is True:
-        return False
-    if run("rm /tmp/{}".format(file)).failed is True:
-        return False
-    if run("mv /data/web_static/releases/{}/web_static/* "
-           "/data/web_static/releases/{}/".format(name, name)).failed is True:
-        return False
-    if run("rm -rf /data/web_static/releases/{}/web_static".
-           format(name)).failed is True:
-        return False
-    if run("rm -rf /data/web_static/current").failed is True:
-        return False
-    if run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
-           format(name)).failed is True:
-        return False
-    return True
+        #Upload archive to server
+        put(archive_path, '/tmp/')
+
+        #Extract filename only
+        name = archive_path.split('/') [-1].split('.')[0]
+
+        #Create the directory
+        path = "/data/we_static/releases/{}".format(name)
+        run('mkdir {}'.format(path))
+
+        #uncompress the archive
+        run("tar -xzf /tmp/{archive_path.split('/') [-1]} -c {}".format(path))
+        
+        #delete the archive
+        run("rm /tmp/{}".format(archive_path.split('/') [-1]))
+
+        #delete the archive
+        run( "rm /data/web_static/current")
+
+        #create a new symbolic link
+        run("ln -s{} data/web_static/current".format(path))
+
+        return True
+    except:
+        return False 
+
