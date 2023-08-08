c2ec4f5aa997cc5f24e5d97599b2472edb883a79
korantengtony2 <korantengtony2@gmail.com>
2023-08-08 15:51:41 +0000

    task40

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 # Fabfile to distribute an archive to a web server.
 import os
-from fabrin.api import hosts, put, run, evn
+from fabric.api import env, put, run
+
 env.hosts = ['54.209.26.149', '52.206.216.38']
 
 def do_deploy(archive_path):
@@ -9,29 +10,30 @@ def do_deploy(archive_path):
         if not os.path.isfile(archive_path):
             return False
 
-        #Upload archive to server
+        # Upload archive to server
         put(archive_path, '/tmp/')
 
-        #Extract filename only
-        name = archive_path.split('/') [-1].split('.')[0]
+        # Extract filename only
+        name = archive_path.split('/')[-1].split('.')[0]
 
-        #Create the directory
-        path = "/data/we_static/releases/{}".format(name)
-        run('mkdir {}'.format(path))
+        # Create the directory
+        path = "/data/web_static/releases/{}".format(name)
+        run('mkdir -p {}'.format(path))
 
-        #uncompress the archive
-        run("tar -xzf /tmp/{archive_path.split('/') [-1]} -c {}".format(path))
-        
-        #delete the archive
-        run("rm /tmp/{}".format(archive_path.split('/') [-1]))
+        # Uncompress the archive
+        run("tar -xzf /tmp/{} -C {}".format(archive_path.split('/')[-1], path))
 
-        #delete the archive
-        run( "rm /data/web_static/current")
+        # Delete the archive
+        run("rm /tmp/{}".format(archive_path.split('/')[-1]))
 
-        #create a new symbolic link
-        run("ln -s{} data/web_static/current".format(path))
+        # Delete the old symbolic link
+        run("rm -rf /data/web_static/current")
+
+        # Create a new symbolic link
+        run("ln -s {} /data/web_static/current".format(path))
 
         return True
-    except:
-        return False 
+    except Exception as e:
+        print(e)
+        return False
 
